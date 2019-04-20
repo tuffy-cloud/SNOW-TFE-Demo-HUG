8d0edd657d9d6c4e71b0758e19748c14daa6f010
Kevin Tuffner <kevin.tuffner@thinkahead.com>
2019-04-19 22:48:45 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,30 @@
-# Configure the AWS Provider
-
+# Create a new instance of the latest Ubuntu 14.04 on an
+# t2.micro node with an AWS Tag naming it "HelloWorld"
 provider "aws" {
-  region     = "us-west-1"
+  region = "us-west-2"
 }
 
-# Create the AMI Instance
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
 
 resource "aws_instance" "web" {
-  ami           = "ami-0ac019f4fcb7cb7e6"
-  instance_type = "t1.micro"
- }
+  ami           = "${data.aws_ami.ubuntu.id}"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
 }

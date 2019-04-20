bcae003f4e9df1e2914d909bae01f9ba26230e44
Kevin Tuffner <kevin.tuffner@thinkahead.com>
2019-04-19 22:47:12 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,9 +9,5 @@ provider "aws" {
 resource "aws_instance" "web" {
   ami           = "ami-0ac019f4fcb7cb7e6"
   instance_type = "t1.micro"
-
-  tags {
-    Name = "demo01"
-    Owner = "tuffner"
-    Dept = "DevOps"
-  }
+ }
+}
